7b88b880bd7419d89b8e836ea3f3d1fa8e7644ea
Jonas Almeida <jalmeida@mathbiol.org>
2021-10-15 17:48:53 -0400

    posting working

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 var http = require("http");
+var fs = require("fs");
 
 //create a server object:
 http
   .createServer(function (req, res) {
-    res.write(`Hello World from Jonas! at ${Date()}`); //write a response to the client
-    res.end(); //end the response
+    console.log(`${Date()}\n`,req)
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    //res.setrHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    if(req.method=="POST"){
+      let body=''
+      req.on('data',function(data){
+        body+=data
+      })
+      req.on('end',function(){
+        let bodyJSON=JSON.parse(body)
+        let filename=body.filename||'hello.json'
+        fs.writeFile(`./data/${filename}`,body,function(err,data){
+          if (err) {
+            return console.log('posted failed:',err);
+          }
+          console.log(`posted to ${filename}:\n`,bodyJSON)
+          res.end(`successufly posted at ${Date()}`)
+        })
+      })
+      //debugger
+    }else{
+      res.write(`Hello World from Jonas :-D ! at ${Date()}`); //write a response to the client
+      res.end(); //end the response
+    }
+      
   })
   .listen(3000); //the server object listens on port 8080
