aa7422cc89592f691aa01edc7d74e76dc1a6f14c
Jonas Almeida <jalmeida@mathbiol.org>
2021-10-16 02:13:13 +0000

    preparing import

--- repl.js
+++ repl.js
@@ -1,12 +1,17 @@
-repl = function(){}
+//repl = function(){}
 
-repl.get = async function (url='http://localhost:3000',format='text'){
+get = async function (url='http://localhost:3000',format='text'){
     return (await fetch(url))[format]()
 }
 
-repl.post = async function (url='http://localhost:3000',data={hello:'world!',at:Date()},format='text'){
+post = async function (url='http://localhost:3000',data={hello:'world!',at:Date()},format='text'){
     return (await fetch(url,{
         method:"POST",
         body:JSON.stringify(data)
     }))[format]()
+}
+
+makeTokens = function(n){
+  return [...Array(n)].
+  map(_=>Math.random().toString().slice(2))
 }
